25fc32e0af008c74309bc3d3bf553b87577c1e8d
Mrflaxe <dmitriyclon@gmail.com>
2022-08-22 07:32:53 +0300

    Now supporting glowing item frames too. Handling unbreaking enchant

--- src/main/java/ru/mrflaxe/invisibleframes/listener/ShearsUsageListener.java
+++ src/main/java/ru/mrflaxe/invisibleframes/listener/ShearsUsageListener.java
@@ -1,9 +1,12 @@
 package ru.mrflaxe.invisibleframes.listener;
 
+import java.util.Random;
+
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.ItemFrame;
@@ -14,7 +17,6 @@ import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ShearsUsageListener implements Listener {
@@ -23,7 +25,8 @@ public class ShearsUsageListener implements Listener {
     public void onShearsUsage(PlayerInteractEntityEvent event) {
         Entity involved = event.getRightClicked();
         
-        if(!involved.getType().equals(EntityType.ITEM_FRAME)) {
+        if(!involved.getType().equals(EntityType.ITEM_FRAME)
+                && !involved.getType().equals(EntityType.GLOW_ITEM_FRAME)) {
             return;
         }
         
@@ -47,18 +50,37 @@ public class ShearsUsageListener implements Listener {
         
         // If player is not in creative mode will damage the shears by 1 value
         if(!player.getGameMode().equals(GameMode.CREATIVE)) {
-            Damageable shearsMeta = (Damageable) item.getItemMeta();
-            int damage = shearsMeta.getDamage();
-            shearsMeta.setDamage(damage + 1);
-            item.setItemMeta((ItemMeta) shearsMeta);
-            
-            player.getInventory().setItem(hand, item);
+            reduceDurability(player, item, hand);
         }
         
         frame.setVisible(false);
         
         Location frameLocation = frame.getLocation();
         frameLocation.getWorld().playSound(frameLocation, Sound.ENTITY_SHEEP_SHEAR, 1, 1);
+    }
+    
+    private void reduceDurability(Player player, ItemStack shears, EquipmentSlot hand) {
+        Damageable shearsMeta = (Damageable) shears.getItemMeta();
+        
+        // If the shears have unbreaking enchantment should to handle it
+        // The chance not to reduce is range of int values. If random 'dice' gets in
+        // this range durability will be decreased.
+        int unbreakingLevel = shears.getItemMeta().getEnchantLevel(Enchantment.DURABILITY);
+        int chanceNotToReduce = 100 / (1 + unbreakingLevel);
+        
+        Random random = new Random();
+        int dice = random.nextInt(100);
+        
+        // For each level of unbreaking chance not to reduce will gets lower and lower
+        // When unbreaking level is 0 chance is 100%
+        // When unbreaking level is 1 chance become 50% and etc.
+        if(dice <= chanceNotToReduce) {
+            int damage = shearsMeta.getDamage();
+            shearsMeta.setDamage(damage + 1);
+            shears.setItemMeta(shearsMeta);
+            
+            player.getInventory().setItem(hand, shears);
+        }
     }
     
     public void register(JavaPlugin plugin) {
